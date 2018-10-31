2d6d8e0cb1b8055718631498daea733817dcbba2
pwy <172226325@qq.com>
2018-10-31 15:32:34 +0800

    local boot conf

--- config/index.js
+++ config/index.js
@@ -28,8 +28,10 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'dev-mall.paascloud.net', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: 'localhost', // can be overwritten by process.env.HOST
+    //host: 'dev-mall.paascloud.net', // can be overwritten by process.env.HOST
+    port: 7030, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    //port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
